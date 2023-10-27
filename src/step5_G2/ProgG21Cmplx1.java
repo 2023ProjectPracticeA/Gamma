c49a56dfbc8d474397ed84368fb94e5b10ce597e
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-28 00:24:41 +0900

    add ProgG21Cmplx1

---
+++ src/step5_G2/ProgG21Cmplx1.java
@@ -0,0 +1,13 @@
+package step5_G2;
+
+/*
+ * 課題番号      ： 第5回 演習問題G2-1
+ * ファイル名    ： ProgG21Cmplx1.java
+ * 作成年月日    ： 2023年10月31日
+ * 学生番号・氏名：
+ * グループ      ： Γ
+ */
+
+public class ProgG21Cmplx1 {
+    
+}
