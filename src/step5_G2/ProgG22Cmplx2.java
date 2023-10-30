1ed403f69f51ebf7ff3231009c4e733e69a0eeb7
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-31 00:40:55 +0900

    add ProgG22Cmplx2

---
+++ src/step5_G2/ProgG22Cmplx2.java
@@ -0,0 +1,13 @@
+package step5_G2;
+
+/*
+ * 課題番号      ： 第5回 演習問題G2-2
+ * ファイル名    ： ProgG22Cmplx2.java
+ * 作成年月日    ： 2023年11月7日
+ * 学生番号・氏名：
+ * グループ      ： Γ
+ */
+
+public class ProgG22Cmplx2 {
+    
+}
