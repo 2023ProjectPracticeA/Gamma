56886ce74dbd871483a5214e3a582f14bc709c54
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-30 23:46:14 +0900

    add complex2

---
+++ src/step5_G2/Complex2.java
@@ -0,0 +1,445 @@
+package step5_G2;
+
+/*
+ * 課題番号      ： 第5回 演習問題G2-2
+ * ファイル名    ： Complex2.java
+ * 作成年月日    ： 2023年11月7日
+ * 学生番号・氏名：
+ * グループ      ： Γ
+ */
+
+/**
+ * 複素数の拡張を表す数を表すクラス
+ */
+public class Complex2 {
+    // 複素数の拡張を表す数の実部
+    private double real = 0.0;
+
+    // 複素数の拡張を表す数の第一虚部
+    private double imag = 0.0;
+
+    // 複素数の拡張を表す数の第二虚部
+    private double j = 0.0;
+
+    // 複素数の拡張を表す数の第三虚部
+    private double k = 0.0;
+
+    /**
+     * コンストラクター
+     */
+    public Complex2() {
+    }
+
+    /**
+     * コンストラクター
+     * 
+     * @param real 実部
+     */
+    public Complex2(double real) {
+        this.real = real;
+    }
+
+    /**
+     * コンストラクター
+     * 
+     * @param real 複素数の実部
+     * @param imag 複素数の虚部
+     */
+    public Complex2(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    /**
+     * コンストラクター
+     * 
+     * @param real 複素数の拡張を表す数の実部
+     * @param imag 複素数の拡張を表す数の第一虚部
+     * @param j    複素数の拡張を表す数の第二虚部
+     * @param k    複素数の拡張を表す数の第三虚部
+     */
+    public Complex2(double real, double imag, double j, double k) {
+        this.real = real;
+        this.imag = imag;
+        this.j = j;
+        this.k = k;
+    }
+
+    /**
+     * コンストラクター
+     * 
+     * @param complex 複素数の拡張を表す数
+     */
+    public Complex2(Complex2 complex) {
+        this.real = complex.real;
+        this.imag = complex.imag;
+        this.j = complex.j;
+        this.k = complex.k;
+    }
+
+    /**
+     * 実部を返す
+     * 
+     * @return 実部
+     */
+    public double getReal() {
+        return this.real;
+    }
+
+    /**
+     * 第一虚部を返す
+     * 
+     * @return 第一虚部
+     */
+    public double getImag() {
+        return this.imag;
+    }
+
+    /**
+     * 第二虚部を返す
+     * 
+     * @return 第二虚部
+     */
+    public double getJ() {
+        return this.j;
+    }
+
+    /**
+     * 第三虚部を返す
+     * 
+     * @return 第三虚部
+     */
+    public double getK() {
+        return this.k;
+    }
+
+    /**
+     * 複素数の拡張を表す数の実部を設定する
+     * 
+     * @param real 複素数の拡張を表す数の実部
+     */
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    /**
+     * 複素数の拡張を表す数の第一虚部を設定する
+     * 
+     * @param imag 複素数の拡張を表す数の第一虚部
+     */
+    public void setImag(double imag) {
+        this.imag = imag;
+    }
+
+    /**
+     * 複素数の拡張を表す数の第二虚部を設定する
+     * 
+     * @param j 複素数の拡張を表す数の第二虚部
+     */
+    public void setJ(double j) {
+        this.j = j;
+    }
+
+    /**
+     * 複素数の拡張を表す数の第三虚部を設定する
+     * 
+     * @param k 複素数の拡張を表す数の第三虚部
+     */
+    public void setK(double k) {
+        this.k = k;
+    }
+
+    /**
+     * 複素数の拡張を表す数の実部と第一虚部を設定する
+     * 
+     * @param real 複素数の拡張を表す数の実部
+     * @param imag 複素数の拡張を表す数の第一虚部
+     */
+    public void set(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    /**
+     * 複素数の拡張を表す数を設定する
+     * 
+     * @param real 複素数の拡張を表す数の実部
+     * @param imag 複素数の拡張を表す数の第一虚部
+     * @param j    複素数の拡張を表す数の第二虚部
+     * @param k    複素数の拡張を表す数の第三虚部
+     */
+    public void set(double real, double imag, double j, double k) {
+        this.real = real;
+        this.imag = imag;
+        this.j = j;
+        this.k = k;
+    }
+
+    /**
+     * 複素数の拡張を表す数を設定する
+     * 
+     * @param complex 複素数の拡張を表す数
+     */
+    public void set(Complex2 complex) {
+        this.real = complex.real;
+        this.imag = complex.imag;
+        this.j = complex.j;
+        this.k = complex.k;
+    }
+
+    /**
+     * 実数と複素数の拡張を表す数の和を返す
+     * 
+     * @param real    実数
+     * @param complex 複素数の拡張を表す数
+     * @return 実数と複素数の拡張を表す数の和
+     */
+    public static Complex2 plus(double real, Complex2 complex) {
+        return new Complex2(real + complex.real, complex.imag, complex.j, complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数と実数の和を返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @param real    実数
+     * @return 複素数の拡張を表す数と実数の和
+     */
+    public static Complex2 plus(Complex2 complex, double real) {
+        return new Complex2(complex.real + real, complex.imag, complex.j, complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数と複素数の拡張を表す数の和を返す
+     * 
+     * @param complex1 複素数の拡張を表す数
+     * @param complex2 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数と複素数の拡張を表す数の和
+     */
+    public static Complex2 plus(Complex2 complex1, Complex2 complex2) {
+        return new Complex2(complex1.real + complex2.real, complex1.imag + complex2.imag, complex1.j + complex2.j,
+                complex1.k + complex2.k);
+    }
+
+    /**
+     * 実数と複素数の拡張を表す数の差を返す
+     * 
+     * @param real    実数
+     * @param complex 複素数の拡張を表す数
+     * @return 実数と複素数の拡張を表す数の差
+     */
+    public static Complex2 minus(double real, Complex2 complex) {
+        return new Complex2(real - complex.real, complex.imag, complex.j, complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数と実数の差を返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @param real    実数
+     * @return 複素数の拡張を表す数と実数の差
+     */
+    public static Complex2 minus(Complex2 complex, double real) {
+        return new Complex2(complex.real - real, complex.imag, complex.j, complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数と複素数の拡張を表す数の差を返す
+     * 
+     * @param complex1 複素数の拡張を表す数
+     * @param complex2 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数と複素数の拡張を表す数の差
+     */
+    public static Complex2 minus(Complex2 complex1, Complex2 complex2) {
+        return new Complex2(complex1.real - complex2.real, complex1.imag - complex2.imag, complex1.j - complex2.j,
+                complex1.k - complex2.k);
+    }
+
+    /**
+     * 実数と複素数の拡張を表す数の積を返す
+     * 
+     * @param real    実数
+     * @param complex 複素数の拡張を表す数
+     * @return 実数と複素数の拡張を表す数の積
+     */
+    public static Complex2 times(double real, Complex2 complex) {
+        return new Complex2(real * complex.real, real * complex.imag, real * complex.j, real * complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数と実数の積を返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @param real    実数
+     * @return 複素数の拡張を表す数と実数の積
+     */
+    public static Complex2 times(Complex2 complex, double real) {
+        return new Complex2(complex.real * real, complex.imag * real, complex.j * real, complex.k * real);
+    }
+
+    /**
+     * 複素数の拡張を表す数と複素数の拡張を表す数の積を返す
+     * 
+     * @param complex1 複素数の拡張を表す数
+     * @param complex2 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数と複素数の拡張を表す数の積
+     */
+    public static Complex2 times(Complex2 complex1, Complex2 complex2) {
+        return new Complex2(complex1.real * complex2.real, complex1.imag * complex2.imag, complex1.j * complex2.j,
+                complex1.k * complex2.k);
+    }
+
+    /**
+     * 実数を複素数の拡張を表す数で割った商を返す
+     * 
+     * @param real    実数
+     * @param complex 複素数の拡張を表す数
+     * @return 実数を複素数の拡張を表す数で割った商
+     */
+    public static Complex2 over(double real, Complex2 complex) {
+        return new Complex2(real / complex.real, real / complex.imag, real / complex.j, real / complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数を実数で割った商を返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @param real    実数
+     * @return 複素数の拡張を表す数を実数で割った商
+     */
+    public static Complex2 over(Complex2 complex, double real) {
+        return new Complex2(complex.real / real, complex.imag / real, complex.j / real, complex.k / real);
+    }
+
+    /**
+     * 複素数の拡張を表す数を複素数の拡張を表す数で割った商を返す
+     * 
+     * @param complex1 複素数の拡張を表す数
+     * @param complex2 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数を複素数の拡張を表す数で割った商
+     */
+    public static Complex2 over(Complex2 complex1, Complex2 complex2) {
+        return new Complex2(complex1.real / complex2.real, complex1.imag / complex2.imag, complex1.j / complex2.j,
+                complex1.k / complex2.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数の絶対値を返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数の絶対値
+     */
+    public static double abs(Complex2 complex) {
+        return Math.sqrt(complex.real * complex.real + complex.imag * complex.imag + complex.j * complex.j
+                + complex.k * complex.k);
+    }
+
+    /**
+     * 複素数の拡張を表す数の絶対値を返す
+     * 
+     * @return 複素数の拡張を表す数の絶対値
+     */
+    public double abs() {
+        return Complex2.abs(this);
+    }
+
+    /**
+     * 複素数の拡張を表す数の偏角を返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数の偏角
+     */
+    public static double arg(Complex2 complex) {
+        return Math.atan2(complex.imag, complex.real);
+    }
+
+    /**
+     * 複素数の拡張を表す数の偏角を返す
+     * 
+     * @return 複素数の拡張を表す数の偏角
+     */
+    public double arg() {
+        return Complex2.arg(this);
+    }
+
+    /**
+     * 複素数の拡張を表す数どうしの値が等しいかどうかを返す
+     * 
+     * @param complex1 複素数の拡張を表す数
+     * @param complex2 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数どうしの値が等しいかどうか
+     */
+    public static boolean equals(Complex2 complex1, Complex2 complex2) {
+        return complex1.real == complex2.real
+                && complex1.imag == complex2.imag 
+                && complex1.j == complex2.j 
+                && complex1.k == complex2.k;
+    }
+
+    /**
+     * 複素数の拡張を表す数どうしの値が等しいかどうかを返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数どうしの値が等しいかどうか
+     */
+    public boolean equals(Complex2 complex) {
+        return Complex2.equals(this, complex);
+    }
+
+    /**
+     * 複素数の拡張を表す数が純粋な実数かどうかを返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数が純粋な実数かどうか
+     */
+    public static boolean isPurelyReal(Complex2 complex) {
+        return complex.imag == 0.0 && complex.j == 0.0 && complex.k == 0.0;
+    }
+
+    /**
+     * 複素数の拡張を表す数が純粋な実数かどうかを返す
+     * 
+     * @return 複素数の拡張を表す数が純粋な実数かどうか
+     */
+    public boolean isPurelyReal() {
+        return Complex2.isPurelyReal(this);
+    }
+
+    /**
+     * 複素数の拡張を表す数が純粋な虚数かどうかを返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数が純粋な虚数かどうか
+     */
+    public static boolean isPurelyImaginary(Complex2 complex) {
+        return complex.real == 0.0 && complex.j == 0.0 && complex.k == 0.0;
+    }
+
+    /**
+     * 複素数の拡張を表す数が純粋な虚数かどうかを返す
+     * 
+     * @return 複素数の拡張を表す数が純粋な虚数かどうか
+     */
+    public boolean isPurelyImaginary() {
+        return Complex2.isPurelyImaginary(this);
+    }
+
+    /**
+     * 複素数の拡張を表す数が純粋な複素数かどうかを返す
+     * 
+     * @param complex 複素数の拡張を表す数
+     * @return 複素数の拡張を表す数が純粋な複素数かどうか
+     */
+    public static boolean isPurelyComplex(Complex2 complex) {
+        return complex.j == 0.0 && complex.k == 0.0;
+    }
+
+    /**
+     * 複素数の拡張を表す数が純粋な複素数かどうかを返す
+     * 
+     * @return 複素数の拡張を表す数が純粋な複素数かどうか
+     */
+    public boolean isPurelyComplex() {
+        return Complex2.isPurelyComplex(this);
+    }
+}
