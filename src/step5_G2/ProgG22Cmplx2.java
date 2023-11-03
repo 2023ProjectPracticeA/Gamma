c84dc750af781ab2bc6b203fcc830d59bd589c37
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-03 22:05:52 +0900

    fix

--- src/step5_G2/ProgG22Cmplx2.java
+++ src/step5_G2/ProgG22Cmplx2.java
@@ -1181,7 +1181,7 @@ public class ProgG22Cmplx2 {
    * テストを実行する
    */
   public static void runTest() {
-      assertEqual("i * i", new Complex(-1), new Complex2(0, 1).times(new Complex2(0, 1)));
+      assertEqual("i * i", new Complex(-1), new Complex2(0, 1).timesPure(new Complex2(0, 1)));
   }
 
   /**
