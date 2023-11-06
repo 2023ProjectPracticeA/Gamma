e16468246100a8255f82176a1a2143bf40ed040b
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-06 22:15:49 +0900

    今日の天気はあめ

--- src/step5_G2/ProgG22Cmplx2.java
+++ src/step5_G2/ProgG22Cmplx2.java
@@ -1177,82 +1177,91 @@ public class ProgG22Cmplx2 {
         }
     }
 
-  /**
-   * テストを実行する
-   */
-  public static void runTest() {
-    //複素数の和
-    assertEqual("r + r, i + i, j + j, k + k", new Complex2(2, 2, 2, 2),new Complex2(1, 1, 1, 1).plusPure(new Complex2(1, 1, 1, 1)));
+    /**
+     * テストを実行する
+     */
+    public static void runTest() {
+        // 複素数の拡張を表す数の和
+        assertEqual("r + r, i + i, j + j, k + k", new Complex2(2, 2, 2, 2),
+                new Complex2(1, 1, 1, 1).plusPure(new Complex2(1, 1, 1, 1)));
 
-	//複素数の差  
-    assertEqual("r - r, i - i, j - j, k - k", new Complex2(0, 0, 0, 0),new Complex2(1, 1, 1, 1).minusPure(new Complex2(1, 1, 1, 1)));
+        // 複素数の拡張を表す数の差
+        assertEqual("r - r, i - i, j - j, k - k", new Complex2(0, 0, 0, 0),
+                new Complex2(1, 1, 1, 1).minusPure(new Complex2(1, 1, 1, 1)));
 
-    //複素数の積
-    assertEqual("i * i", new Complex2(-1), new Complex2(0, 1).timesPure(new Complex2(0, 1)));
-    
-    assertEqual("j * j", new Complex2(-1), new Complex2(0, 0, 1, 0).timesPure(new Complex2(0, 0, 1, 0)));
-    
-    assertEqual("k * k", new Complex2(-1), new Complex2(0, 0, 0, 1).timesPure(new Complex2(0, 0 ,0 ,1)));
-    
-    assertEqual("i * j * k", new Complex2(-1), new Complex2(0, 1, 0, 0).timesPure
-            (new Complex2(0, 0 ,1 ,0).timesPure(new Complex2(0, 0 ,0 ,1))));
+        // 複素数の拡張を表す数の積
+        assertEqual("i * i", new Complex2(-1), new Complex2(0, 1).timesPure(new Complex2(0, 1)));
 
-    //複素数の商
-    assertEqual("i / i", new Complex2(1),new Complex2(0 , 1, 0, 0).overPure(new Complex2(0, 1, 0, 0)));
-    
-    assertEqual("j / j", new Complex2(1),new Complex2(0, 0, 1, 0).overPure(new Complex2(0, 0, 1, 0)));
-    
-    assertEqual("k / k", new Complex2(1),new Complex2(0, 0, 0, 1).overPure(new Complex2(0, 0, 0, 1)));
-    
-    //共役複素数
-    assertEqual("r = i = j = k = 1 の共役複素数", new Complex2(1,-1,-1,-1),new Complex2(1,1,1,1).conjPure());
-    
-    //絶対値
-    assertEqual2("r = i = j = k = 1 の絶対値", 2 ,new Complex2(1,1,1,1).abs());
-    
-    //偏角
-    assertEqual2("real = 1 img = 1の偏角", 0.78,new Complex2(1,1).arg());
-    
-    //内積
-    assertEqual2("real = 1, img = 1の内積", 2,new Complex2(1,1).dot(new Complex2(1,1)));
-    
-  }
-  /**
-   * 複素数の拡張を表す数の、期待値と実際の値が等しいかを確認する
-   * 
-   * @param message  表示するメッセージ
-   * @param expected 期待値
-   * @param actual   実際の値
-   */
-  public static void assertEqual(String message, Complex2 expected, Complex2 actual) {
-    // 期待値と実際の値が等しいかを確認
-    if (expected.equals(actual)) {
-      // 等しい場合は OK と表示
-      System.out.println("OK: " + message);
-    } else {
-      // 等しくない場合は NG と表示
-      System.out.println("NG: " + message);
+        assertEqual("j * j", new Complex2(-1), new Complex2(0, 0, 1, 0).timesPure(new Complex2(0, 0, 1, 0)));
 
-      // 期待値と実際の値を表示
-      System.out.println("  期待: " + expected.toString());
-      System.out.println("  実際: " + actual.toString());
+        assertEqual("k * k", new Complex2(-1), new Complex2(0, 0, 0, 1).timesPure(new Complex2(0, 0, 0, 1)));
+
+        assertEqual("i * j * k", new Complex2(-1),
+                new Complex2(0, 1, 0, 0).timesPure(new Complex2(0, 0, 1, 0).timesPure(new Complex2(0, 0, 0, 1))));
+
+        // 複素数の拡張を表す数の商
+        assertEqual("i / i", new Complex2(1), new Complex2(0, 1, 0, 0).overPure(new Complex2(0, 1, 0, 0)));
+
+        assertEqual("j / j", new Complex2(1), new Complex2(0, 0, 1, 0).overPure(new Complex2(0, 0, 1, 0)));
+
+        assertEqual("k / k", new Complex2(1), new Complex2(0, 0, 0, 1).overPure(new Complex2(0, 0, 0, 1)));
+
+        // 共役
+        assertEqual("r = i = j = k = 1 の共役", new Complex2(1, -1, -1, -1), new Complex2(1, 1, 1, 1).conjPure());
+
+        // 絶対値
+        assertEqual("r = i = j = k = 1 の絶対値", 2, new Complex2(1, 1, 1, 1).abs());
+
+        // 偏角
+        assertEqual("real = 1 img = 1 の偏角", Math.PI / 4, new Complex2(1, 1).arg());
+
+        // 内積
+        assertEqual("real = 1, img = 1 どうしの内積", 2, new Complex2(1, 1).dot(new Complex2(1, 1)));
     }
-  }
 
-  public static void assertEqual2(String message, double expected, double actual) {
-    // 期待値と実際の値が等しいかを確認
-    if (expected == actual) {
-      // 等しい場合は OK と表示
-      System.out.println("OK: " + message);
-    } else {
-      // 等しくない場合は NG と表示
-      System.out.println("NG: " + message);
+    /**
+     * 複素数の拡張を表す数の、期待値と実際の値が等しいかを確認する
+     * 
+     * @param message  表示するメッセージ
+     * @param expected 期待値
+     * @param actual   実際の値
+     */
+    public static void assertEqual(String message, Complex2 expected, Complex2 actual) {
+        // 期待値と実際の値が等しいかを確認
+        if (expected.equals(actual)) {
+            // 等しい場合は OK と表示
+            System.out.println("OK: " + message);
+        } else {
+            // 等しくない場合は NG と表示
+            System.out.println("NG: " + message);
 
-      // 期待値と実際の値を表示
-      System.out.println("  期待: " + expected);
-      System.out.println("  実際: " + actual);
+            // 期待値と実際の値を表示
+            System.out.println("  期待: " + expected.toString());
+            System.out.println("  実際: " + actual.toString());
+        }
     }
-}
+
+    /**
+     * 複素数の拡張を表す数の、期待値と実際の値が等しいかを確認する
+     * 
+     * @param message  表示するメッセージ
+     * @param expected 期待値
+     * @param actual   実際の値
+     */
+    public static void assertEqual(String message, double expected, double actual) {
+        // 期待値と実際の値が等しいかを確認
+        if (expected == actual) {
+            // 等しい場合は OK と表示
+            System.out.println("OK: " + message);
+        } else {
+            // 等しくない場合は NG と表示
+            System.out.println("NG: " + message);
+
+            // 期待値と実際の値を表示
+            System.out.println("  期待: " + expected);
+            System.out.println("  実際: " + actual);
+        }
+    }
 
     public class Vector3 {
         public double x, y, z;
