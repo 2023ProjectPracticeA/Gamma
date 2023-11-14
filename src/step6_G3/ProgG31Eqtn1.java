ba7c0e36c5ef255643493723045a9705785fe5db
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-14 14:34:58 +0900

    add ProgG22Cmplx2

---
+++ src/step6_G3/ProgG31Eqtn1.java
@@ -0,0 +1,13 @@
+package step6_G3;
+
+/*
+ * 課題番号      ： 第7回 演習問題G3-1
+ * ファイル名    ： ProgG31Eqtn1.java
+ * 作成年月日    ： 2023年11月14日
+ * 学生番号・氏名：
+ * グループ      ： Γ
+ */
+
+public class ProgG31Eqtn1 {
+    
+}
